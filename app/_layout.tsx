dcaf2196e2d980023d0665d0b6c87935866b4807
Moeen Mahmud <write2fahim007@gmail.com>
2026-01-19 20:15:29 +0600

    feat: adds component library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from "expo-router"
 import "../global.css"
 
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider"
+import { StatusBar } from "expo-status-bar"
+
 export default function RootLayout() {
-    return <Stack />
+    return (
+        <GluestackUIProvider>
+            <StatusBar style="auto" />
+            <Stack />
+        </GluestackUIProvider>
+    )
 }
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,21 @@
 module.exports = function (api) {
     api.cache(true)
+
     return {
-        presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
-        plugins: ["react-native-reanimated/plugin"],
+        presets: [["babel-preset-expo"], "nativewind/babel"],
+
+        plugins: [
+            [
+                "module-resolver",
+                {
+                    root: ["./"],
+                    alias: {
+                        "@": "./",
+                        "tailwind.config": "./tailwind.config.js",
+                    },
+                },
+            ],
+            "react-native-worklets/plugin",
+        ],
     }
 }
